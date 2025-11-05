99aab9f976c8cf35c8e6426dfa45e2ae93df7abd
raapeli <aapeli@rautiainen.info>
2025-11-05 17:03:41 +0200

    Add support for our first instruction.
    Next step is to reformat whole project to a
    header etc. to make further instructions more
    straightforward to add.
    
    Before that I probably need to implement
    special registers for I/O operations etc.
    
    Signed-off-by: raapeli <aapeli@rautiainen.info>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include<bits/stdc++.h>
 #include <iostream>
 #include <vector>
+#include <format>
 
 
-std::vector<int32_t> registers;
+
+//TODO: More instructions and also reformat whole file to a header
+enum instructions {
+    ADDI,
+    EBREAK
+};
+
+std::array<int, 32> registers;
+std::map<std::string, int32_t> register_map;
+
+instructions str_to_instructions(const std::string& instruction) {
+
+    if (instruction == "addi") {
+        return ADDI;
+    }
+    return EBREAK;
+}
+
+
+void addi(std::vector<std::string>& args) {
+    const int dest_reg = register_map[args[1].substr(0, 3)];
+    const int source_reg = register_map[args[2].substr(0, 3)];
+    const int immediate_reg = static_cast<int>(strtoul(args[3].c_str(), nullptr, 16));
+
+    registers[dest_reg] =  registers.at(source_reg) + immediate_reg;
+}
+
 int main() {
 
     std::ifstream f("testi.txt");
@@ -22,19 +49,32 @@ int main() {
     std::stringstream ss(s);
     char del = ' ';
     std::string instruction;
+
+
+    registers.fill(0);
+
+    for (int i = 0; i < 32; i++) {
+        register_map[std::format("x{:02}", i)] = i;
+    }
+
     while (getline(ss, instruction, del)) {
-        instruction.append(instruction);
+        if (!instruction.empty())
+            instructions.push_back(instruction);
     }
 
     //TODO: Make an enum of instructions and a mapping function
-    switch (instructions[0]) {
-        case 'addi':
+    switch (str_to_instructions(instructions[0])) {
+        case ADDI:
+            addi(instructions);
 
+        default:
+            NULL;
 
-        default: ;
     }
 
-    registers.reserve(32);
+    for (int i = 0; i < registers.size(); i++) {
+        std::cout << std::format("(x{:02}): {:x}\n", i, registers.at(i));
+    }
 
     return 0;
 }
