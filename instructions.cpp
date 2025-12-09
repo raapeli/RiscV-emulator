5cc007f54793fd146f4ae776ac43b2a68e389156
raapeli <aapeli@rautiainen.info>
2025-12-09 22:54:52 +0200

    Added some new instructions.
    Next up is making some tests for them
    
    Signed-off-by: raapeli <aapeli@rautiainen.info>

--- instructions.cpp
+++ instructions.cpp
@@ -1,19 +1,114 @@
 #include "instructions.h"
+#include <cstdint>
 
 instructions str_to_instructions(const std::string &instruction) {
-
-  if (instruction == "addi") {
+  if (instruction == "and") {
+    return AND;
+  } else if (instruction == "andi") {
+    return ANDI;
+  } else if (instruction == "or") {
+    return OR;
+  } else if (instruction == "ori") {
+    return ORI;
+  } else if (instruction == "xor") {
+    return XOR;
+  } else if (instruction == "xori") {
+    return XORI;
+  } else if (instruction == "add") {
+    return ADD;
+  } else if (instruction == "addi") {
     return ADDI;
   } else if (instruction == "sub") {
     return SUB;
+  } else if (instruction == "slt") {
+    return SLT;
+  } else if (instruction == "sltu") {
+    return SLTU;
+  } else if (instruction == "slti") {
+    return SLTI;
+  } else if (instruction == "sltiu") {
+    return SLTIU;
+  } else if (instruction == "srl") {
+    return SRL;
+  } else if (instruction == "sra") {
+    return SRA;
+  } else if (instruction == "srli") {
+    return SRLI;
+  } else if (instruction == "srai") {
+    return SRAI;
+  } else if (instruction == "ebreak") {
+    return EBREAK;
   }
-  return EBREAK;
+  return EBREAK; // Default for invalid instructions
 }
 
+// LOGICAL OPERATORS
+void andf(int *regs, int dest, int reg1, int reg2) {
+  regs[dest] = regs[reg1] & regs[reg2];
+}
+void andi(int *regs, int dest, int reg1, int imm) {
+  regs[dest] = regs[reg1] & imm;
+}
+void orf(int *regs, int dest, int reg1, int reg2) {
+  regs[dest] = regs[reg1] | regs[reg2];
+}
+void xorf(int *regs, int dest, int reg1, int reg2) {
+  regs[dest] = regs[reg1] ^ regs[reg2];
+}
+void ori(int *regs, int dest, int reg1, int imm) {
+  regs[dest] = regs[reg1] or imm;
+}
+void xori(int *regs, int dest, int reg1, int imm) {
+  regs[dest] = regs[reg1] ^ imm;
+}
+
+// ARTIHMETIC OPERATORS
+void add(int *regs, int dest, int reg1, int reg2) {
+  regs[dest] = regs[reg1] + regs[reg2];
+}
 void addi(int *regs, int dest, int reg1, int imm) {
   regs[dest] = regs[reg1] + imm;
 }
-
 void sub(int *regs, int dest, int reg1, int reg2) {
   regs[dest] = regs[reg1] - regs[reg2];
+}
+
+// COMPARISONS
+void slt(int *regs, int dest, int reg1, int reg2) {
+  if ((int32_t)regs[reg1] < (int32_t)regs[reg2])
+    regs[dest] = 1;
+  else
+    regs[dest] = 0;
+}
+void sltu(int *regs, int dest, int reg1, int reg2) {
+  if ((u_int32_t)regs[reg1] < (u_int32_t)regs[reg2])
+    regs[dest] = 1;
+  else
+    regs[dest] = 0;
+}
+void slti(int *regs, int dest, int reg1, int imm) {
+  if ((int32_t)regs[reg1] < (int32_t)imm)
+    regs[dest] = 1;
+  else
+    regs[dest] = 0;
+}
+void sltiu(int *regs, int dest, int reg1, int imm) {
+  if ((u_int32_t)regs[reg1] < (u_int32_t)imm)
+    regs[dest] = 1;
+  else
+    regs[dest] = 0;
+}
+
+// SHIFTS
+void srl(int *regs, int dest, int reg1, int reg2) {
+  regs[dest] = (u_int32_t)regs[reg1] >> regs[reg2];
+}
+void sra(int *regs, int dest, int reg1, int reg2) {
+  regs[dest] = (int32_t)regs[reg1] >> regs[reg2];
+}
+void srli(int *regs, int dest, int reg1, int imm) {
+  regs[dest] = (u_int32_t)regs[reg1] >> imm;
+}
+void srai(int *regs, int dest, int reg1, int imm) {
+  regs[dest] = (int32_t)regs[reg1] >> imm;
 }
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <format>
 #include <fstream>
 #include <iostream>
@@ -7,8 +8,28 @@
 
 #include "instructions.h"
 
-int registers[31] = {0};
+int32_t registers[31] = {0};
 std::map<std::string, int32_t> register_map;
+
+void prepImm(std::queue<std::string> *q, int32_t *dest, int32_t *reg1,
+             int32_t *imm) {
+  *dest = register_map[q->front().substr(0, 3)];
+  q->pop();
+  *reg1 = register_map[q->front().substr(0, 3)];
+  q->pop();
+  *imm = std::stoi(q->front(), 0, 16);
+  q->pop();
+}
+
+void prep2reg(std::queue<std::string> *q, int32_t *dest, int32_t *reg1,
+              int32_t *reg2) {
+  *dest = register_map[q->front().substr(0, 3)];
+  q->pop();
+  *reg1 = register_map[q->front().substr(0, 3)];
+  q->pop();
+  *reg2 = register_map[q->front().substr(0, 3)];
+  q->pop();
+}
 
 int main() {
 
@@ -20,7 +41,7 @@ int main() {
   }
 
   // TODO: Clean up
-  std::queue<std::string> instructions;
+  std::queue<std::string> inst_q;
   char del = ' ';
 
   for (int i = 0; i < 32; i++) {
@@ -36,40 +57,109 @@ int main() {
     std::string token;
     while (std::getline(line_ss, token, del)) {
       if (!token.empty()) {
-        instructions.push(token);
+        inst_q.push(token);
       }
     }
   }
 
-  while (!instructions.empty()) {
+  int32_t dest, reg1, reg2, imm = 0;
+  while (!inst_q.empty()) {
     // TODO: Make an enum of instructions and a mapping function
-    auto inst = str_to_instructions(instructions.front());
-    instructions.pop();
+    auto inst = str_to_instructions(inst_q.front());
+    inst_q.pop();
 
+    dest = 0;
+    reg1 = 0;
+    reg2 = 0;
+    imm = 0;
     switch (inst) {
+    case AND: {
+      prep2reg(&inst_q, &dest, &reg1, &reg2);
+      andf(registers, dest, reg1, reg2);
+      break;
+    }
+    case ANDI: {
+      prepImm(&inst_q, &dest, &reg1, &imm);
+      andi(registers, dest, reg1, imm);
+      break;
+    }
+    case OR: {
+      prep2reg(&inst_q, &dest, &reg1, &reg2);
+      orf(registers, dest, reg1, reg2);
+      break;
+    }
+    case ORI: {
+      prepImm(&inst_q, &dest, &reg1, &imm);
+      ori(registers, dest, reg1, reg2);
+      break;
+    }
+    case XOR: {
+      prep2reg(&inst_q, &dest, &reg1, &reg2);
+      xorf(registers, dest, reg1, reg2);
+      break;
+    }
+    case XORI: {
+      prepImm(&inst_q, &dest, &reg1, &imm);
+      xori(registers, dest, reg1, reg2);
+      break;
+    }
 
+    case ADD: {
+      prep2reg(&inst_q, &dest, &reg1, &reg2);
+      add(registers, dest, reg1, imm);
+      break;
+    }
     case ADDI: {
-      auto dest = register_map[instructions.front().substr(0, 3)];
-      instructions.pop();
-      auto reg1 = register_map[instructions.front().substr(0, 3)];
-      instructions.pop();
-      auto imm = std::stoi(instructions.front(), 0, 16);
-      instructions.pop();
-
+      prepImm(&inst_q, &dest, &reg1, &imm);
       addi(registers, dest, reg1, imm);
       break;
     }
     case SUB: {
-      auto dest = register_map[instructions.front().substr(0, 3)];
-      instructions.pop();
-      auto reg1 = register_map[instructions.front().substr(0, 3)];
-      instructions.pop();
-      auto reg2 = register_map[instructions.front().substr(0, 3)];
-      instructions.pop();
-
+      prep2reg(&inst_q, &dest, &reg1, &reg2);
       sub(registers, dest, reg1, reg2);
       break;
     }
+    case SLT: {
+      prep2reg(&inst_q, &dest, &reg1, &reg2);
+      slt(registers, dest, reg1, reg2);
+      break;
+    }
+    case SLTU: {
+      prep2reg(&inst_q, &dest, &reg1, &reg2);
+      sltu(registers, dest, reg1, reg2);
+      break;
+    }
+    case SLTI: {
+      prepImm(&inst_q, &dest, &reg1, &imm);
+      slti(registers, dest, reg1, imm);
+      break;
+    }
+    case SLTIU: {
+      prepImm(&inst_q, &dest, &reg1, &imm);
+      sltiu(registers, dest, reg1, imm);
+      break;
+    }
+    case SRL: {
+      prep2reg(&inst_q, &dest, &reg1, &reg2);
+      srl(registers, dest, reg1, reg2);
+      break;
+    }
+    case SRA: {
+      prep2reg(&inst_q, &dest, &reg1, &reg2);
+      sra(registers, dest, reg1, reg2);
+      break;
+    }
+    case SRLI: {
+      prepImm(&inst_q, &dest, &reg1, &imm);
+      srli(registers, dest, reg1, imm);
+      break;
+    }
+    case SRAI: {
+      prepImm(&inst_q, &dest, &reg1, &imm);
+      srai(registers, dest, reg1, imm);
+      break;
+    }
+
     default:
       NULL;
     }
