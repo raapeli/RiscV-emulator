6260b1e58871ff8aed7edfcd27d482fb11e5a2bc
raapeli <aapeli@rautiainen.info>
2025-12-30 19:06:32 +0200

    Simple main.cpp used for testing
    Signed-off-by: raapeli <aapeli@rautiainen.info>

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
+#include "bus.h"
+#include "cpu.h"
 #include "parser.h"
+#include <cstddef>
+#include <cstdint>
 #include <fstream>
+#include <ios>
 #include <iostream>
+#include <vector>
 int main(int argc, char *argv[]) {
 
   if (argc != 2) {
@@ -13,11 +19,26 @@ int main(int argc, char *argv[]) {
     std::cerr << "File reading failed\n";
     return -1;
   }
-  std::string source((std::istreambuf_iterator<char>(file)),
-                     std::istreambuf_iterator<char>());
-  source += '\n';
+
+  file.seekg(0, std::ios::end);
+  const auto size = file.tellg();
+  file.seekg(0, std::ios::beg);
+
+  std::vector<uint8_t> data(static_cast<std::size_t>(size));
+  file.read(reinterpret_cast<char *>(data.data()), size);
 
   std::cout << "Parsed succesfully\n";
 
+  file.close();
+
+  Bus *bus = new Bus();
+  XRegisters *xregs = new XRegisters();
+  Cpu *cpu = new Cpu(xregs, bus);
+
+  for (unsigned long i = 0; i < data.size(); i++) {
+    bus->write(DRAM_BASE + i, BYTE, data[i]);
+  }
+
+  cpu->start();
   return 0;
 }
